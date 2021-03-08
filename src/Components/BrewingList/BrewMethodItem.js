9cd105f5ac34de8d938aca75f9246b32df0edea4
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-03-08 09:51:05 -0800

    Brought in the BrewingMethodModal into the BrewMethodItem.js

--- src/Components/BrewingList/BrewMethodItem.js
+++ src/Components/BrewingList/BrewMethodItem.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+
+// Component
+import BrewingMethodModal from "../BrewingMethodModal/index";
 
 // Styling
 import "../../style.css";
+import "../../Modal.css";
 
 const BrewMethodItem = ({ brewingMethod }) => {
+    const [modalState, setModalState] = useState(false);
+
+    const handleToggle = () => {
+        setModalState(!modalState);
+    };
+
   return (
-    <div>
+    <div onClick={handleToggle}>
       <h3 className="font-weight-medium text-center text-lg-left mt-4 mb-0">
         {brewingMethod.name}
       </h3>
-      <hr className="mt-2 mb-5" />
+      <hr className="mt-2 mb-4" />
       <div className="row text-center text-lg-left">
         <div className="box">
           <img
@@ -19,8 +29,23 @@ const BrewMethodItem = ({ brewingMethod }) => {
             style={{ height: 160, width: 170 }}
           />
           <div>
-            <button className="btn">Brew</button>
+            <button 
+            id="#btn"
+            type="button"
+            className="btn"
+            data-toggle="modal"
+            data-target="#brewingModal"
+            
+            >
+                Brew
+                </button>
           </div>
+          {/* For the Modal */}
+          <BrewingMethodModal
+          handleToggle={handleToggle}
+          brewingMethod={brewingMethod}
+          modalState={modalState}
+          />
         </div>
       </div>
     </div>
