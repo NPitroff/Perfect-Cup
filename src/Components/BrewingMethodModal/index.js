619b55313c5366918a8672ea039128daa67b776a
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-03-08 14:31:58 -0800

    Added the AmountCalculator to the BrewingMethodModal

--- src/Components/BrewingMethodModal/index.js
+++ src/Components/BrewingMethodModal/index.js
@@ -2,6 +2,7 @@ import React from "react";
 //Components
 import { Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 import BrewingMethodDetails from "../BrewingMethodDetails/index";
+import AmountCalculator from "../AmountCalculator/index";
 import CupSize from "../CupSize/index";
 //Styling
 import "../../style.css"
@@ -12,6 +13,7 @@ const BrewingMethodModal = ({ brewingMethod, handleToggle, modalState }) => {
       <Modal size="lg" isOpen={modalState} toggle={handleToggle}>
         <ModalHeader toggle={handleToggle}>{brewingMethod.name}</ModalHeader>
         <ModalBody>
+          <AmountCalculator brewingMethod={brewingMethod} />
             <BrewingMethodDetails brewingMethod={brewingMethod} />
             <CupSize />
         </ModalBody>
