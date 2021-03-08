8e64a9b13b8791373205de57ef868c40b613bb98
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-03-08 09:37:56 -0800

    Added a BrewMethodItem file to the BrewingList folder in the components

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   const [brewingMethods] = useState(allBrewMethods);
   const [showHome, setShowHome] = useState(true);
   // Display the brewing methods for the coffee
-  const methodList = brewingMethods.map(method => (
+  const methodList = brewingMethods.map((method) => (
     <div className="col-4">
       <h3>{method.name}</h3>
       <img src={method.imageUrl} alt={method.name} className="mx-auto" />
@@ -29,11 +29,11 @@ function App() {
       </div>
       <NavBar setShowHome={setShowHome} />
       <div className="App my-5">
-      {showHome ? (
-            <BrewingList brewingMethods={brewingMethods} />
-          ) : (
-            <AboutPage />
-          )}
+        {showHome ? (
+          <BrewingList brewingMethods={brewingMethods} />
+        ) : (
+          <AboutPage />
+        )}
       </div>
     </div>
   );
