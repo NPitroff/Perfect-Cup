b66d04d0bc999aaf26896a7b2c59049dee6a7a87
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-03-08 09:35:46 -0800

    Added code to display the brewing methods in the App.js file

--- src/App.js
+++ src/App.js
@@ -7,21 +7,35 @@ import "./App.css";
 import allBrewMethods from "./data";
 
 //Components
+import BrewingList from "./Components/BrewingList/index.js";
 import AboutPage from "./Components/AboutPage";
 import NavBar from "./Components/NavBar";
 
 function App() {
+  const [brewingMethods] = useState(allBrewMethods);
   const [showHome, setShowHome] = useState(true);
+  // Display the brewing methods for the coffee
+  const methodList = brewingMethods.map(method => (
+    <div className="col-4">
+      <h3>{method.name}</h3>
+      <img src={method.imageUrl} alt={method.name} className="mx-auto" />
+    </div>
+  ));
 
   return (
-    <>
+    <div className="App my-5">
+      <div className="container">
+        <div className="row">{methodList}</div>
+      </div>
       <NavBar setShowHome={setShowHome} />
       <div className="App my-5">
-        <div className="container">{showHome ? null : <AboutPage />}</div>
-        {/* START BY SETTING UP THE MBD class divisions */}
-
+      {showHome ? (
+            <BrewingList brewingMethods={brewingMethods} />
+          ) : (
+            <AboutPage />
+          )}
       </div>
-    </>
+    </div>
   );
 }
 
