d15be1ee7b02a5614bf5b4715b067be3deb41d76
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-03-08 14:03:46 -0800

    moved the brewing details into their own component

---
+++ src/Components/BrewingMethodDetails/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+//Styling
+import "../../style.css";
+
+const BrewingMethodDetails = ({ brewingMethod }) => {
+    return (
+        <div className="wrapper-details">
+                <div className="modal-box">
+                    <div>Grind Size: </div> <br />
+                    <div>{brewingMethod.grind_size}</div>
+                </div>
+                <div className="modal-box">
+                    <div>Brew Time:</div> <br />
+                    <div> {brewingMethod.total_time} minutes </div>
+                </div>
+                <div className="modal-box">
+                    <div>Water:</div> <br />
+                    <div> {brewingMethod.default_water}ml</div>
+                </div>
+            </div>
+    );
+};
