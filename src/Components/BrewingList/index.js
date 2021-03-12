b956dad9305e023af1c897449f84cd758a2424a6
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-03-11 20:17:59 -0800

    Fixed syntax in the instructions index.js

--- src/Components/BrewingList/index.js
+++ src/Components/BrewingList/index.js
@@ -8,9 +8,11 @@ import "../../style.css";
 
 const BrewingList = ({ brewingMethods }) => {
   const methodList = brewingMethods.map(method => (
-    <div className="col-4">
+    <div className="col-4" >
       <BrewMethodItem brewingMethod={method} />
+      
       {/* <img src={method.imageUrl} alt={method.name} className="mx-auto" /> */}
+      {/* ^^Results in a doubled image that makes the page look messy^^ */}
     </div>
   ));
 
