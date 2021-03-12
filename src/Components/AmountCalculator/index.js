6f4f96b40f56a8c5c571693107df1d88248cbca2
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-03-11 20:37:33 -0800

    Fixed the layout of the AmountCalculator

--- src/Components/AmountCalculator/index.js
+++ src/Components/AmountCalculator/index.js
@@ -1,4 +1,4 @@
-import React,{ useState } from "react";
+import React, { useState } from "react";
 
 //Components
 import Slider from "@material-ui/core/Slider";
@@ -9,7 +9,7 @@ import CupSize from "../CupSize/index";
 import "../../style.css";
 
 const AmountCalculator = ({ brewingMethod }) => {
-    const [amount, setAmount] = useState();
+  const [amount, setAmount] = useState();
   const [grams, setGrams] = useState();
   const [water, setWater] = useState();
   const [selectedOption, setSelectedOption] = useState(227);
@@ -17,70 +17,72 @@ const AmountCalculator = ({ brewingMethod }) => {
   const marks = [
     {
       value: 12,
-      label: "12",
+      label: "12"
     },
     {
       value: 13,
-      label: "13",
+      label: "13"
     },
     {
       value: 14,
-      label: "14",
+      label: "14"
     },
     {
       value: 15,
-      label: "15",
-    },
+      label: "15"
+    }
   ];
   const handleChange = () => {
     setGrams(parseInt(selectedOption / amount));
     setWater(parseInt(grams * 15));
   };
 
-  const handleRatio = (value) => {
-      setAmount(value);
-      handleChange();
+  const handleRatio = value => {
+    setAmount(value);
+    handleChange();
     return JSON.stringify(value);
   };
 
   return (
-    <div className="wrapper-details-input">
-      <div>
-        <h5 className="text-center">Coffee to Water Ratio: </h5>
+    <div>
+      <div className="wrapper-details-input">
+        <div>
+          <h5 className="text-center">Coffee to Water Ratio: </h5>
+        </div>
+        <div>
+          <h6 className="text-center">stronger</h6>
+        </div>
+        <Slider
+          orientation="horizontal"
+          defaultValue={15}
+          min={12}
+          max={15}
+          getAriaValueText={handleRatio}
+          step={1}
+          marks={marks}
+          valueLabelDisplay="on"
+          style={{
+            width: "200px",
+            color: "#8cb9fd"
+          }}
+        />
+        <div>
+          <h6 className="text-center">lighter</h6>
+        </div>
       </div>
-      <div>
-        <h6 className="text-center">stronger</h6>
-      </div>
-      <Slider
-        orientation="horizontal"
-        defaultValue={15}
-        min={12}
-        max={15}
-        getAriaValueText={handleRatio}
-        step={1}
-        marks={marks}
-        valueLabelDisplay="on"
-        style={{
-          width: "200px",
-          color: "#8cb9fd",
-        }}
+      <hr className="mt-2 mb-4" />
+      <CupSize
+        handleChange={handleChange}
+        selectedOption={selectedOption}
+        setSelectedOption={setSelectedOption}
       />
-      <div>
-        <h6 className="text-center">lighter</h6>
-      </div>
+      <hr className="mt-2 mb-4" />
 
-      <hr className="mt-2 mb-4" />
-      <CupSize 
-      handleChange={handleChange}
-      selectedOption={selectedOption}
-      setSelectedOption={setSelectedOption}
+      <BrewingMethodDetails
+        brewingMethod={brewingMethod}
+        grams={grams}
+        water={water}
       />
-      <hr className="mt-2 mb-4" />
-      <BrewingMethodDetails 
-      brewingMethod={brewingMethod}
-      grams={grams}
-      water={water}
-       />
     </div>
   );
 };
